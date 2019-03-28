58fc6d07ef4d5d9b977b75186ab0c111906c3a4b
dianlujitao <dianlujitao@lineageos.org>
2019-03-28 14:33:56 +0800

    sdm660-common: Enable OEM unlock and bootable image signing
     * Given the fact that the bootloader allows bootable images with
       arbitrary signatures to boot, one can relock the bootloader after
       installing a signed custom recovery. After that, custom ROMs can be
       installed from recovery as usual, as long as the boot.img's are
       signed.
     * This is NOT a security vulnerability because unlocked bootloader is
       still prerequisite to access fastboot boot/flash, in contrast it's a
       feature suggested by Google.
     * To make it feasible with Lineage, sign the img with AOSP verity key
       which is publicly available.
     * This doesn't mean using custom ROMs with locked bootloader is
       suggested.
    
    Change-Id: I178e9588e1dde96400dcb2178a027597d05949bd
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- platform.mk
+++ platform.mk
@@ -270,6 +270,10 @@ PRODUCT_PACKAGES += \
 	libandroid_net \
 	netutils-wrapper-1.0
 
+# OEM Unlock reporting
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.oem_unlock_supported=1
+
 # OMX
 PRODUCT_PACKAGES += \
 	libc2dcolorconvert \
