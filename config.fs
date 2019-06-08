PixelExperience-Devices/device_motorola_sdm660-common

sdm660-common: config.fs: Add mapping for sensors daemon
Set filesystem configuration for sensors daemon binary.

Change-Id: I499ad9b987a43e79b8037da5761e14e97b2ef9c9
Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

# Conflicts:
#	config.fs

config.fs
@@ -88,6 +88,12 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND
 
+[vendor/bin/sensors.qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [firmware/]
 mode: 0771
 user: AID_SYSTEM

StagOS-Devices/android_device_xiaomi_sdm660-common

config.fs
@@ -157,6 +157,12 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
+[vendor/bin/sensors.qcom]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [firmware/]
 mode: 0771
 user: AID_SYSTEM