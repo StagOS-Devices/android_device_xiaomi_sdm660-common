f4e67dd08c442a28193f32dccfc1c8d77ee02be7
Stylogey <stylogey@gmail.com>
2019-06-13 21:26:59 +0200

    sdm660: Add dirac [2/2]

--- platform.mk
+++ platform.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
 	com.quicinc.cne \
 	services-ext
 
+# Dirac
+PRODUCT_PACKAGES += \
+	Dirac
+
 # Display
 PRODUCT_PACKAGES += \
 	android.hardware.graphics.allocator@2.0-impl:64 \
