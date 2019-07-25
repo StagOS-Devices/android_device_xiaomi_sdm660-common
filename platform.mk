ec4b58eceaef83546fa56f2a54d80a46cce225ca
PythonLimited <leonardomeitz@gmail.com>
2019-07-25 16:48:27 +0200

    sdm660-common: remove MiuiCamera

--- platform.mk
+++ platform.mk
@@ -29,7 +29,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product-if-exists, build/target/product/embedded.mk)
-$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
 # Vendor files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
